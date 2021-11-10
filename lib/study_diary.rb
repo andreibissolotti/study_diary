1c92a68c9ba126af6df977f6e72ee1525b3aede1
andreibissolotti <andreibisso@gmail.com>
2021-11-10 19:47:12 -0300

    Add create option

--- lib/study_diary.rb
+++ lib/study_diary.rb
@@ -1,13 +1,29 @@
-def menu
+@itens = []
+
+def get_options
   options = <<~OPTIONS
-  Cadastrar um item para estudar
-  Ver todos os itens cadastrados
-  Buscar um item de estudo
-  Sair
+    Cadastrar um item para estudar
+    Ver todos os itens cadastrados
+    Buscar um item de estudo
+    Sair
   OPTIONS
 
-  options_array = options.split("\n")
-  
+  options.split("\n")
+end
+
+def get_categorys
+  categorys = <<~CATEGORYS
+    Ruby
+    Rails
+    HTML
+    Javascript
+  CATEGORYS
+
+  categorys.split("\n")
+end
+
+def menu
+  options_array = get_options
   options_array.each_with_index{ |text, index| puts "[#{ index + 1 }] #{ text }" }
   puts "Escolha uma opção:"
 
@@ -17,8 +33,43 @@ def menu
     puts "Opção inválida, escolha novamente:"
     input = gets.chomp
   end
-  @option = input
+  @option = input.to_i
 end
 
-menu
-puts @option
+def create
+  puts "Digite o titulo do item:"
+  title = gets.chomp
+
+  puts "==============================="
+
+  categorys_array = get_categorys
+  categorys_array.each_with_index{ |text, index| puts "##{ index + 1 } - #{ text }" }
+  puts "Defina a categoria:"
+  
+  valid_categorys = (1..categorys_array.length).to_a
+  input = gets.chomp
+  until valid_categorys.include?(input.to_i)
+    puts "Categoria inválida, escolha novamente:"
+    input = gets.chomp
+  end
+  category = input
+
+  @itens << {category: category, title: title}
+end
+
+
+
+
+
+begin
+  puts `clear`
+  menu
+
+  case @option
+  when 1
+    create
+    puts "Pressione 'Enter' para continuar"
+    gets
+  end
+end until @option == 4
+puts "Obrigado por usar o diario de estudos"
